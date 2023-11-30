bee2470e42b3b7608249166e0b842a0f8a4cc569
Riccardo Nestola <riccardinux@gmail.com>
2023-11-30 17:29:16 +0100

    add tags

--- src/assets/components/Cards/Card.jsx
+++ src/assets/components/Cards/Card.jsx
@@ -11,7 +11,7 @@ const Card = () =>{
                 <div className="container px-5 py-24 mx-auto" >
                     <div className="flex flex-wrap -m-4">
 
-                        {posts.map((post) => (             
+                        {posts.map((post) => (       
                             post.published && <CardInfo key={post.id} {...post} />
                         ))}
                     </div>
--- src/assets/components/Cards/CardInfo.jsx
+++ src/assets/components/Cards/CardInfo.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const TagColor = {
+    "html": "bg-red-500",
+    "css": "bg-blue-500",
+    "js": "bg-yellow-500",
+    "php": "bg-violet-500"
+};
 
 const CardInfo = (props) => {
 
@@ -10,7 +16,11 @@ const CardInfo = (props) => {
                     <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden" >
                         <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={props.image} alt="blog" />
                         <div className="p-6">
-                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{props.tags.join(" ")}</h2>
+                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                                {props.tags.map(tag => (
+                                    <span key={tag} className={`${TagColor[tag]} mr-2 p-1 rounded text-white`}>{tag}</span>
+                                ))}
+                            </h2>
                             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{props.title}</h1>
                             <p className="leading-relaxed mb-3">{props.content}</p>
                             <div className="flex items-center flex-wrap ">
